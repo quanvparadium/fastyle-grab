0d1e61af3a31ff34c2dbe6279b63a3c8c7366c8e
Tran Binh Kha <tbkha1609@gmail.com>
2024-05-04 03:11:49 +0700

    feat: handle ui retrival tab

---
+++ frontend/src/app/(dashboard)/components/Header.tsx
@@ -0,0 +1,26 @@
+import { icons } from 'lucide-react'
+import React from 'react'
+
+interface HeaderProps {
+  title: string
+  description: string
+  icon: keyof typeof icons
+}
+
+const Header: React.FC<HeaderProps> = ({ title, description, icon }) => {
+  const LucideIcon = icons[icon]
+
+  return (
+    <div className='px-4 flex items-center gap-x-3 mb-8'>
+      <div className='p-2 w-fit rounded-md bg-pink-700/10'>
+        <LucideIcon className='w-10 h-10 text-pink-700' />
+      </div>
+      <div>
+        <h2 className='text-3xl font-bold'>{title}</h2>
+        <p className='text-sm text-muted-foreground'>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
