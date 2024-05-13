6b5299c7fb2b6cac117e9ec7ce3e17d9fb447811
Tran Binh Kha <94106543+trkha16@users.noreply.github.com>
2024-05-13 14:50:26 +0700

    feat: render toolbar when click object (#14)

--- frontend/src/store/tryonStore.ts
+++ frontend/src/store/tryonStore.ts
@@ -3,13 +3,21 @@ import { create } from 'zustand'
 
 interface TryOnOutfitState {
   tryOnOutfit: TryOnOutfit | null
+  isChangeViewBtnDisable: boolean
+
   setTryOnOutfit: (newTryOnOutfit: TryOnOutfit) => void
+  setIsChangeViewBtnDisable: (status: boolean) => void
 }
 
 const useTryOnOutfitStore = create<TryOnOutfitState>()((set) => ({
   tryOnOutfit: null,
+  isChangeViewBtnDisable: true,
+
   setTryOnOutfit: (newTryOnOutfit) => {
     return set({ tryOnOutfit: newTryOnOutfit })
+  },
+  setIsChangeViewBtnDisable: (status) => {
+    return set({ isChangeViewBtnDisable: status })
   },
 }))
 
