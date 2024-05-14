b225074ae1f9450e25fb400f9ef0ef46614e17f1
Tran Binh Kha <tbkha1609@gmail.com>
2024-05-14 09:51:15 +0700

    fix: text selected image in right side

--- frontend/src/app/(dashboard)/suggestion/components/Form.tsx
+++ frontend/src/app/(dashboard)/suggestion/components/Form.tsx
@@ -1,27 +1,14 @@
 'use client'
 
-import PreviewChosen from '@/app/(dashboard)/suggestion/components/PreviewChosen'
+import PreviewSection from '@/app/(dashboard)/suggestion/components/PreviewSection'
 import SelectOutfitSection from '@/app/(dashboard)/suggestion/components/SelectOutfitSection'
-import { Button } from '@/components/ui/button'
 
 const Form = () => {
   return (
     <div className='h-[550px] border-2 shadow-sm rounded-2xl flex'>
       <SelectOutfitSection />
 
-      <div className='flex-1 flex flex-col px-4 py-4'>
-        <div className='flex-1 flex flex-col gap-9 overflow-hidden pb-4'>
-          <div className='w-full flex items-center justify-center'>
-            <span className='text-[20px] font-medium'>Your Item</span>
-          </div>
-          <PreviewChosen />
-        </div>
-        <div className='w-full flex justify-end'>
-          <Button className='w-[130px]' hasShadow>
-            Submit
-          </Button>
-        </div>
-      </div>
+      <PreviewSection />
     </div>
   )
 }
---
+++ frontend/src/app/(dashboard)/suggestion/components/PreviewSection.tsx
@@ -0,0 +1,23 @@
+import PreviewChosen from '@/app/(dashboard)/suggestion/components/PreviewChosen'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const PreviewSection = () => {
+  return (
+    <div className='flex-1 flex flex-col px-4 py-4'>
+      <div className='flex-1 flex flex-col gap-9 overflow-hidden pb-4'>
+        <div className='w-full flex items-center justify-center'>
+          <span className='text-[20px] font-medium'>Your Item</span>
+        </div>
+        <PreviewChosen />
+      </div>
+      <div className='w-full flex justify-end'>
+        <Button className='w-[130px]' hasShadow>
+          Submit
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default PreviewSection
