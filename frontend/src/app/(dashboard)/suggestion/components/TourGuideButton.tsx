b385dc6657801ba3cd884a729c4fc554ff1a2056
nmkha-github <nguyenminhkha2001@gmail.com>
2024-05-16 00:43:47 +0700

    feat: suggestion tutorial

---
+++ frontend/src/app/(dashboard)/suggestion/components/TourGuideButton.tsx
@@ -0,0 +1,109 @@
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Dialog, DialogContent } from '@radix-ui/react-dialog'
+import { useMemo, useState } from 'react'
+import Joyride, { Callback, Step, TooltipRenderProps } from 'react-joyride'
+
+const TourGuideButton = () => {
+  const [guideRunning, setGuideRunning] = useState(false)
+  const steps: Array<Step> = useMemo(
+    () => [
+      {
+        content: 'Welcome to suggestion section tour guide',
+        placement: 'center',
+        target: 'body',
+      },
+      {
+        target: '.step-1',
+        content: 'abc cakasdcn asdkcnaksjcdnkj ákcn jnacsjcsn s',
+        placement: 'bottom',
+      },
+      { target: '.step-2', content: 'abc', placement: 'right' },
+      { target: '.step-3', content: 'abc', placement: 'left' },
+      { target: '.step-4', content: 'abc', placement: 'top' },
+    ],
+    [],
+  )
+
+  const handleGuideClose: Callback = (data) => {
+    if (data.status === 'finished') {
+      setGuideRunning(false)
+    }
+  }
+
+  return (
+    <div>
+      <Button
+        className='items-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+        onClick={() => setGuideRunning(true)}
+      >
+        TOUR GUIDE
+      </Button>
+
+      <Joyride
+        locale={{
+          back: 'previous step',
+          last: 'last',
+          next: 'next step',
+        }}
+        callback={handleGuideClose}
+        run={guideRunning}
+        steps={steps}
+        continuous
+        disableScrolling
+        showProgress
+        tooltipComponent={Tooltip}
+      />
+    </div>
+  )
+}
+
+export default TourGuideButton
+
+const Tooltip = ({
+  continuous,
+  index,
+  step,
+  size,
+  backProps,
+  closeProps,
+  primaryProps,
+  tooltipProps,
+}: TooltipRenderProps) => (
+  <div
+    className='flex-col justify-between'
+    style={{
+      maxWidth: 400,
+      minHeight: 100,
+      padding: '16px 32px',
+      background: 'white',
+      borderRadius: 16,
+    }}
+    {...tooltipProps}
+  >
+    {step.title && <div>{step.title}</div>}
+    <div>{step.content}</div>
+    <div style={{ textAlign: 'end', marginTop: 16 }}>
+      {index > 0 && (
+        <Button
+          className='mr-2 outline-none items-center bg-transparent text-black font-semibold hover:text-gray-600 py-2 px-4 hover:border-transparent rounded'
+          {...backProps}
+        >
+          <div id='back' />
+          Previous step
+        </Button>
+      )}
+      {continuous && (
+        <Button {...primaryProps}>
+          <div id='next' />
+          {index > 0 ? `Step ${index}/${size - 1}` : 'Next'}
+        </Button>
+      )}
+      {!continuous && (
+        <Button {...closeProps}>
+          <div id='close' />
+        </Button>
+      )}
+    </div>
+  </div>
+)
--- frontend/src/app/(dashboard)/suggestion/page.tsx
+++ frontend/src/app/(dashboard)/suggestion/page.tsx
@@ -1,16 +1,21 @@
+'use client'
 import Header from '@/app/(dashboard)/components/Header'
 import Form from '@/app/(dashboard)/suggestion/components/Form'
 import ListSuggestOutfit from '@/app/(dashboard)/suggestion/components/ListSuggestOutfit'
 import React from 'react'
+import TourGuideButton from './components/TourGuideButton'
 
 const Suggestion = () => {
   return (
     <div className='w-full pt-6 px-10'>
-      <Header
-        title='Fashion Suggestion'
-        description='Discover your ideal outfit instantly with image or text-based search.'
-        icon={'Lightbulb'}
-      />
+      <div className='flex justify-between items-center'>
+        <Header
+          title='Fashion Suggestion'
+          description='Discover your ideal outfit instantly with image or text-based search.'
+          icon={'Lightbulb'}
+        />
+        <TourGuideButton />
+      </div>
 
       <div className='flex flex-col gap-6'>
         <Form />
