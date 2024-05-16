66bd21ab561d164073f8e4521c692e9f9fafb356
quanvparadium <quan.vparoger@gmail.com>
2024-05-16 14:34:32 +0700

    return Outfit Recommendation url

--- backend/src/controllers/recommend.controllers.ts
+++ backend/src/controllers/recommend.controllers.ts
@@ -4,8 +4,9 @@ import HTTPSTATUS from '~/constants/httpStatus'
 import { RecommendReqBody } from '~/models/requests/request'
 import dotenv from 'dotenv'
 import databaseService from '~/services/database.services'
-import { ObjectId } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import Clothes from '~/models/schemas/Clothes.schema'
+import { axiosPython } from '~/constants/axios'
 dotenv.config()
 
 export const recommendRandomController = async (
@@ -34,15 +35,37 @@ export const recommendController = async (
     next: NextFunction
 ) => {
     const keys = Object.keys(req.body)
-    const result: object = {}
+    const queryPython: object = {}
     for (const key of keys) {
         const values = req.body[key]
-        const objectIds = values.map((id: string)=> new ObjectId(id))
-        const infos = await databaseService[key].find({ _id: { $in: objectIds }}).toArray()
-        result[key] = infos.map((info: Clothes) => info.clothId)
+        const objectIds = values.map((id: string) => new ObjectId(id))
+        const infos = await databaseService[key].find({ _id: { $in: objectIds } }).toArray()
+        queryPython[key] = infos.map((info: Clothes) => info.clothId)
     }
+    // console.log(typeof (queryPython as any).topwear)
+    // console.log((queryPython as any).topwear)
+    // for (const value of (queryPython as any).topwear) {
+    //     console.log('Value: ', typeof value)
+    // }
+    const outfits = await axiosPython.post('/api/recommended', JSON.parse(JSON.stringify(queryPython)))
+    const data = outfits?.data
+    const result: object[] = []
+    for (const outfit of data.outfit) {
+        const viewOutfit: object = {}
+        for (const key of Object.keys(outfit)) {
+            const garment: Clothes = await (databaseService[key] as Collection<Clothes>).findOne({
+                clothId: outfit[key]
+            })
+            // console.log(garment)
+            const imageUrl: object = {}
+            imageUrl['original'] = garment.view
+            viewOutfit[key] = imageUrl
+        }
+        result.push(viewOutfit)
+    }
+
     return res.status(HTTPSTATUS.ACCEPTED).json({
-        message: 'Return outfit successfully',
-        result
+        message: data.message,
+        outfit: result
     })
 }
--- services/app/routes/recommended.py
+++ services/app/routes/recommended.py
@@ -6,9 +6,8 @@ import numpy as np
 recommendedRouter = APIRouter(prefix="/recommended")
 
 @recommendedRouter.post('/')
+async def recommend(input: RecommendedInput):
 
-
-def recommend(input: RecommendedInput):
     dictionary = dict()
     features = dict()
     index_features = dict()
@@ -17,15 +16,15 @@ def recommend(input: RecommendedInput):
         if category[1] is None:
             continue
         dictionary[category[0]] = category[1]
-        print(category)
+        # print(category)
     image_id = load_json_path('../infos/image_id.json')
     for category in dictionary.keys():
         values = dictionary[category]
         for idx, value in enumerate(values):
-            index_val = image_id['{}.npy'.format(value//1000)].index('{}.jpg'.format(value))
-            print("Index value: ", index_val)
-            data = np.load('../features/BLIP_raw_features/{}.npy'.format(value//1000))
-            print(data[index_val].shape)
+            index_val = image_id['{}.npy'.format((value - 1)//1000)].index('{}.jpg'.format(value))
+            # print("Index value: ", index_val)
+            data = np.load('../features/BLIP_raw_features/{}.npy'.format((value - 1)//1000))
+            # print(data[index_val].shape)
             if category in features:
                 features[category].append(data[index_val])
                 index_features[category].append(idx)
@@ -33,19 +32,19 @@ def recommend(input: RecommendedInput):
                 features[category] = [data[index_val]]
                 index_features[category] = [0]
                 order_category.append(category)
-    for category in features:
-        for feat in features[category]:
-            print(f"Category {category} shape: ", feat.shape)
+    # for category in features:
+    #     for feat in features[category]:
+    #         print(f"Category {category} shape: ", feat.shape)
     from itertools import product
     combinations = [list(combo) for combo in product(*index_features.values())]
     top_3_scores = outfit_recommend(features=features, combinations=combinations, order=order_category)
     top_3_outfit = []
     for scores in top_3_scores:
-        outfit = []
+        outfit = dict()
         for idx, order in enumerate(order_category):
-            outfit.append(dictionary[order][scores[idx]])
+            outfit[order] = dictionary[order][scores[idx]]
         top_3_outfit.append(outfit)
     return { 
-        "message": "Default recommended" ,
+        "message": "Outfit is recommended successfully" ,
         "outfit": top_3_outfit
     }
