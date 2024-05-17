619662086784e5cc23b9a719fb1eb61ac1264f93
Tran Binh Kha <94106543+trkha16@users.noreply.github.com>
2024-05-17 12:53:59 +0700

    feat: handle try on using ai (#32)

--- frontend/src/store/tryOnAIStore.ts
+++ frontend/src/store/tryOnAIStore.ts
@@ -3,20 +3,26 @@ import { create } from 'zustand'
 interface TryOnOutfitAIState {
   modelUrl: string | null
   clothesUrl: string | null
+  resultUrl: string | null
 
-  setModelUrl: (data: string) => void
+  setModelUrl: (data: string | null) => void
   setClothesUrl: (data: string) => void
+  setResultUrl: (data: string) => void
 }
 
 const useTryOnOutfitAIStore = create<TryOnOutfitAIState>()((set) => ({
   clothesUrl: null,
   modelUrl: null,
+  resultUrl: null,
 
   setClothesUrl: (data) => {
     return set({ clothesUrl: data })
   },
   setModelUrl: (data) => {
     return set({ modelUrl: data })
+  },
+  setResultUrl: (data) => {
+    return set({ resultUrl: data })
   },
 }))
 
