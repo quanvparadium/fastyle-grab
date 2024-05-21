c0ad6bc557e8e0b813b2da0456d9940c6b1e3f3a
Tran Binh Kha <tbkha1609@gmail.com>
2024-05-21 21:04:11 +0700

    feat: handle try on ai bottom

--- frontend/src/services/tryOnAI/createTryOnOutfitAI.tsx
+++ frontend/src/services/tryOnAI/createTryOnOutfitAI.tsx
@@ -1,57 +1,32 @@
 import { useToast } from '@/components/ui/use-toast'
-import { API_KEY, FASHION_ENDPOINT, fashionParams } from '@/constants/try-on-ai'
+import axiosInstance from '@/lib/axios'
 import useTryOnOutfitAIStore from '@/store/tryOnAIStore'
 import { useMutation } from '@tanstack/react-query'
-import axios, { AxiosResponse } from 'axios'
+import { AxiosResponse } from 'axios'
 
 type FashionResponse = {
-  status: 'processing'
-  fetch_result: string
+  message: string
+  image_url: string
 }
 
-const fetchFashion = async ({
+const fetchTryOn = async ({
   clothesUrl,
   modelUrl,
 }: {
   clothesUrl: string
   modelUrl: string
 }) => {
-  const response: AxiosResponse<FashionResponse> = await axios.post(
-    FASHION_ENDPOINT,
+  const response: AxiosResponse<FashionResponse> = await axiosInstance.post(
+    '/try-on',
     {
-      ...fashionParams,
-      cloth_image: clothesUrl,
-      init_image: modelUrl,
+      clothUrl: clothesUrl,
+      base_url: modelUrl,
+      clothId: Math.floor(Math.random() * (5000 - 1 + 1)) + 1,
     },
   )
 
   console.log('fetchFashion', response.data)
   return response.data
-}
-
-const fetchResult = async (fetchUrl: string) => {
-  const response = await axios.post(fetchUrl, {
-    key: API_KEY,
-  })
-
-  console.log('fetchResult', response.data)
-  return response.data
-}
-
-const fetchWithRetry = async (
-  fetchUrl: string,
-  retriesLeft: number = 6,
-): Promise<any> => {
-  const response = await fetchResult(fetchUrl)
-
-  if (response?.status === 'success') {
-    return response
-  } else if (response.status === 'processing' && retriesLeft > 0) {
-    await new Promise((resolve) => setTimeout(resolve, 10000)) // Đợi 5 giây trước khi fetch lại
-    return fetchWithRetry(fetchUrl, retriesLeft - 1)
-  } else {
-    throw new Error('Failed to fetch result or status is not success.')
-  }
 }
 
 const useCreateTryOnOutfitAI = () => {
@@ -61,7 +36,7 @@ const useCreateTryOnOutfitAI = () => {
 
   return useMutation({
     mutationFn: () =>
-      fetchFashion({
+      fetchTryOn({
         clothesUrl: clothesUrl as string,
         modelUrl: modelUrl as string,
       }),
@@ -69,19 +44,7 @@ const useCreateTryOnOutfitAI = () => {
       setIsLoadingResult(true)
     },
     onSuccess: async (data) => {
-      if (data && data.fetch_result) {
-        try {
-          const response = await fetchWithRetry(data.fetch_result)
-          console.log('result', response)
-          setResultUrl(response?.output[0])
-        } catch (error) {
-          toast({
-            variant: 'destructive',
-            description: `Something went wrong!`,
-            duration: 4000,
-          })
-        }
-      }
+      setResultUrl(data?.image_url)
     },
     onError: (error) => {
       toast({
