a222a747c3c0e14dc200927779fd8d6a18ae756f
nmkha-github <nguyenminhkha2001@gmail.com>
2024-05-21 12:41:28 +0700

    Error page

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,7 +1,7 @@
 'use client' // Error components must be Client Components
 
 import { useEffect } from 'react'
-
+import { Button } from '@/components/ui/button'
 export default function Error({
   error,
   reset,
@@ -15,16 +15,17 @@ export default function Error({
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <section className='flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700'>
+      <div className='container flex flex-col items-center '>
+        <div className='flex flex-col gap-6 max-w-md text-center'>
+          <h2 className='font-extrabold text-9xl text-gray-600 dark:text-gray-100'>
+            <span className='sr-only'>Error</span>404
+          </h2>
+          <p className='text-2xl md:text-3xl dark:text-gray-300'>
+            Sorry, we couldn't find this page.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
